3cf6d5a57d685b277ac9eba6ff4f6db79d5dc6fd
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2024-11-04 08:22:59 +0530

    Completed the basics

--- React Intro/practice.js
+++ React Intro/practice.js
@@ -2,32 +2,41 @@ function Header() {
   return (
     <header>
       <nav>
+        <div className='nav'>
         <img
           src="https://img.freepik.com/free-vector/white-frames-watercolour-texture_1048-11051.jpg?t=st=1730529563~exp=1730533163~hmac=77b48abd63cc25cf96edb78b52e2d042e8def44cded29419e8e580bc2ee068b5&w=740"
-          width="40px"
-        />
+          className="img"
+          />
+          <div className='list'>
+            <ul>
+              <li>Pricing</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          </div>
       </nav>
     </header>
   );
 }
 
-const heading = <h1> My reasons to learn react</h1>;
+const heading = <h1 className='heading'> My reasons to learn react</h1>;
 
 function List() {
   return (
     <>
-      <ul>
+      <ol className='reasons'>
         <li>To understand Working of React more deeply...</li>
         <li>To get fluent in implementing React...</li>
         <li>To build something valueable out of React...</li>
-      </ul>
+      </ol>
     </>
   );
 }
 
 function Footer() {
   return (
-    <footer>
+    <footer className='footer'>
       <small>cprt 2024, All rights Reserved</small>
     </footer>
   );
